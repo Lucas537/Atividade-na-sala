fcfe69f51b5ee065a79d3a8832f03f86ccfca4e5
Lucas537 <ll6767248@gmail.com>
2024-09-09 23:08:54 -0300

    atividade

--- calculadora.js
+++ calculadora.js
@@ -1,21 +1,21 @@
-function insert(num) {
-    document.getElementById("display").value += num;
-}
+const { default: expect } = require("expect");
+const Calculadora = require("./calculadora2");
 
-function clearDisplay() {
-    document.getElementById("display").value = "";
-}
+describle("Calculadora", () => {
+    it("Criar nova Calculadora", () => {
+        const calculadora = new Calculadora();
+        expect(calculadora).toBeDefined();
+    })
 
-function deleteLast() {
-    var display = document.getElementById("display").value;
-    document.getElementById("display").value = display.substring(0, display.length - 1);
-}
-
-function calculate() {
-    var display = document.getElementById("display").value;
-    try {
-        document.getElementById("display").value = eval(display);
-    } catch (e) {
-        document.getElementById("display").value = "Erro";
-    }
-}
+    it("Operação soma", () => {
+        const calculadora = new Calculadora();
+        expect(calculadora.soma).toBeDefined();
+        expect(clculadora.soma.length).toBe(1);
+        expect(() => calculadora.soma("Texto")).toThrow(TypeError);
+        expect(() => calculadora.soma(true)).toThrow("O argumento deve ser um numero válido")
+        expect(() => calculadora.soma("5")).not.toThrow(TypeError);
+        expect(typeof calculadora.resultado).toBe('number');
+        expect(calculadora.resultado).toBe(5);
+        
+    })
+})
---
+++ calculadora2.js
@@ -0,0 +1,33 @@
+class Calculadora {
+    #resultado;
+
+    constructor() {
+        this.#resultado = 0;
+    }
+
+    get resultado() {
+        return this.#resultado;
+    }
+
+    set (resultado){
+        if (typeof numero == 'string'){
+            numero = numero - 0;
+        }
+        if (isNaN(numero) || typeof numero !== 'number') {
+            throw new TypeError(" deve ser um número válido")
+        }
+        this.#resultado = numero;
+    }
+
+    soma(numero) {
+        if (typeof numero == 'string') {
+            numero = numero - 0;
+        }
+        if (isNaN(numero) || typeof numero !== 'number') {
+            throw new TypeError(" deve ser um número válido")
+        }
+        this.#resultado += numero;
+    }
+}
+
+module.exports = Calculadora;
